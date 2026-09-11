8cec201349210bb943e1e8bc24bc227e68439f5d
Jyrki Heikkinen <jyrki.j.heikkinen@gmail.com>
2026-09-11 07:25:46 +0300

    refactor: assume the same order of fields in both CSV files

--- trf/csv.py
+++ trf/csv.py
@@ -1,46 +1,30 @@
 import csv
 
-from enum import IntEnum
-
 from trf.args import RATINGS_FILE
 from trf.player import Player
 
-class Registrants(IntEnum):
-    FIRST_NAME = 0
-    LAST_NAME = 1
-    CLUB = 2
-    RATING = 3
+def parse_rated_players(filename = RATINGS_FILE) -> list[Player]:
+    number_of_extra_fields = 3
+    return __parse_csv(filename, ';', number_of_extra_fields)
 
-class Ratings(IntEnum):
-    LAST_NAME = 3
-    FIRST_NAME = 4
-    CLUB = 5
-    RATING = 6
+def parse_registrants(filename: str) -> list[Player]:
+    return __parse_csv(filename)
 
-def parse_rated_players(csv_file = RATINGS_FILE) -> list[Player]:
-    with open(csv_file, encoding='utf-8') as file:
-        reader = csv.reader(file, delimiter=';')
+def __parse_csv(filename: str, delimiter = ',', extra_fields = 0) -> list[Player]:
+    LAST_NAME = 0 + extra_fields
+    FIRST_NAME = 1 + extra_fields
+    CLUB = 2 + extra_fields
+    RATING = 3 + extra_fields
+
+    with open(filename, encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter=delimiter)
         next(reader) # skip header
         result = []
         for row in reader:
             result.append(Player(
-                row[Ratings.FIRST_NAME],
-                row[Ratings.LAST_NAME],
-                row[Ratings.RATING],
-                row[Ratings.CLUB],
-            ))
-        return result
-
-def parse_registrants(csv_file: str) -> list[Player]:
-    with open(csv_file, encoding='utf-8') as file:
-        reader = csv.reader(file)
-        next(reader) # skip header
-        result = []
-        for row in reader:
-            result.append(Player(
-                row[Registrants.FIRST_NAME],
-                row[Registrants.LAST_NAME],
-                row[Registrants.RATING] if len(row) > Registrants.RATING else '',
-                row[Registrants.CLUB],
+                row[FIRST_NAME],
+                row[LAST_NAME],
+                row[RATING] if len(row) > RATING else '',
+                row[CLUB],
             ))
         return result
