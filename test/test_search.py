932dd4719fb7b13da5b6f4d3bb070f0e0b70176b
Jyrki Heikkinen <jyrki.j.heikkinen@gmail.com>
2026-09-11 20:21:15 +0300

    style: add type hint

--- test/test_search.py
+++ test/test_search.py
@@ -11,10 +11,10 @@ TEST_DATA = [
     {'name': 'h'},
 ]
 
-def test_binary_search_found():
+def test_binary_search_found() -> None:
     result = binary_search(TEST_DATA, 'g', key=lambda x: x['name'])
     assert result == 6
 
-def test_binary_search_not_found():
+def test_binary_search_not_found() -> None:
     result = binary_search(TEST_DATA, 'foo', key=lambda x: x['name'])
     assert result == -1
