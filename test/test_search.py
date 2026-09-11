2fffdaeb5abb51ec54210c42e3b9512a3520f57f
Jyrki Heikkinen <jyrki.j.heikkinen@gmail.com>
2026-09-11 20:10:11 +0300

    test: verify binary_search

---
+++ test/test_search.py
@@ -0,0 +1,20 @@
+from trf.search import binary_search
+
+TEST_DATA = [
+    {'name': 'a'},
+    {'name': 'b'},
+    {'name': 'c'},
+    {'name': 'd'},
+    {'name': 'e'},
+    {'name': 'f'},
+    {'name': 'g'},
+    {'name': 'h'},
+]
+
+def test_binary_search_found():
+    result = binary_search(TEST_DATA, 'g', key=lambda x: x['name'])
+    assert result == 6
+
+def test_binary_search_not_found():
+    result = binary_search(TEST_DATA, 'foo', key=lambda x: x['name'])
+    assert result == -1
