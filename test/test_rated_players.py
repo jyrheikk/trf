34321faa44de72eff1125eac35e0e371e1319a3a
Jyrki Heikkinen <jyrki.j.heikkinen@gmail.com>
2026-09-17 21:46:03 +0300

    test: fix tests

--- test/test_rated_players.py
+++ test/test_rated_players.py
@@ -6,15 +6,11 @@ TEST_RATINGS = 'test/data/test-ratings.csv'
 def test_search_all() -> None:
     rated = RatedPlayers(TEST_RATINGS)
     participants = [
-        Player('Aarre', 'Aalto'),
-        Player('Erkki', 'Aalto', 'HämSK'),
         Player('jyrki', 'heikkinen', '', 'lauttssk'),
         Player('Somebody', 'Not Rated')
     ]
     expected = [
         Player('Jyrki', 'Heikkinen', '2064', 'LauttSSK'),
-        Player('Erkki', 'Aalto', '1949', 'HämSK'),
-        Player('Somebody', 'Not Rated', '1525'),
-        Player('Aarre', 'Aalto', '1409'),
+        Player('Somebody', 'Not Rated', '1525')
     ]
-    result = rated.search_all(participants)
+    assert rated.search_all(participants) == expected
