d53641f89159d793e02c9a53ff1823f71ec7577f
Jyrki Heikkinen <jyrki.j.heikkinen@gmail.com>
2026-09-17 20:00:15 +0300

    feat: exclude unofficial clubs and remove inactivity sign

--- trf/player.py
+++ trf/player.py
@@ -2,13 +2,16 @@ from trf.search import binary_search
 
 DEFAULT_RATING = '1525'
 
+UNOFFICIAL_CLUB = '/'
+INACTIVE_SUFFIX = ' #'
+
 class Player:
     def __init__(self, first_name: str, last_name: str, rating = DEFAULT_RATING, club = ''):
         self.first_name = first_name
         self.last_name = last_name
         self.rating = rating
-        self.club = club
-        club_info = f'({club.lower()})' if club else ''
+        self.club = Player.parse_club(club)
+        club_info = f'({self.club.lower()})' if self.club else ''
         self.search_name = f'{last_name.lower()}, {first_name.lower()} {club_info}'
         self.is_new = False
 
@@ -27,3 +30,12 @@ class Player:
             key=lambda x: x.search_name
         )
         return players[i] if i > -1 else None
+
+    @staticmethod
+    def parse_club(club: str) -> str:
+        if UNOFFICIAL_CLUB in club:
+            return ''
+        elif club.endswith(INACTIVE_SUFFIX):
+            return club[:-len(INACTIVE_SUFFIX)]
+        else:
+            return club
