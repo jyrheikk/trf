873d55638b243ae8e6163d4c46898763afafdc18
Jyrki Heikkinen <jyrki.j.heikkinen@gmail.com>
2026-09-22 22:57:14 +0300

    test: fix tests

--- test/test_player.py
+++ test/test_player.py
@@ -1,32 +1,6 @@
-import pytest
-from pytest import CaptureFixture
-
 from trf.player import Player
-
-RATINGS = [
-    Player('Jyrki', 'Heikkinen', '2064', 'LauttSSK'),
-    Player('Jyrki', 'Heikkinen', '1602', 'MatSK'),
-    Player('Random', 'Surname', '1500', 'X')
-]
 
 def test_set_needs_license() -> None:
     p = Player('random', 'surname')
     p.set_needs_license()
     assert p.needs_license
-
-def test_search_succeeds_with_only_name() -> None:
-    participant = Player('random', 'surname', '', 'Wrong Club')
-    player = participant.search(RATINGS, only_name=True)
-    assert player == Player('Random', 'Surname', '1500', 'X')
-
-def test_search_fails_with_incorrect_club() -> None:
-    participant = Player('random', 'surname', '', 'club typo')
-    assert not participant.search(RATINGS)
-
-def test_search_fails_with_duplicate_names(capsys: CaptureFixture[str]) -> None:
-    expected = 'Duplicates found'
-    participant = Player('jyrki', 'heikkinen')
-    with pytest.raises(SystemExit) as exit_msg:
-        participant.search(RATINGS, only_name=True)
-    output = capsys.readouterr()
-    assert expected in output.err or expected in output.out or expected in str(exit_msg.value)
--- test/test_rating_list.py
+++ test/test_rating_list.py
@@ -1,10 +1,14 @@
+import pytest
+from pytest import CaptureFixture
+
 from trf.player import Player
 from trf.rating_list import RatingList
 
 TEST_RATINGS = 'test/data/test-ratings.csv'
 
+rating_list = RatingList(TEST_RATINGS)
+
 def test_search() -> None:
-    rating_list = RatingList(TEST_RATINGS)
     participants = [
         Player('jyrki', 'heikkinen', '', 'lauttssk'),
         Player('Somebody', 'Not Rated')
@@ -14,3 +18,20 @@ def test_search() -> None:
         Player('Somebody', 'Not Rated', '1525')
     ]
     assert rating_list.search_all(participants) == expected
+
+def test_search_succeeds_with_only_name() -> None:
+    participant = Player('erkki', 'aalto', '', 'Wrong Club')
+    player = rating_list.search_unique(participant, only_name=True)
+    assert player == Player('Erkki', 'Aalto', '1629', 'HämSK')
+
+def test_search_fails_with_incorrect_club() -> None:
+    participant = Player('erkki', 'aalto', '', 'club typo')
+    assert not rating_list.search_unique(participant)
+
+def test_search_fails_with_duplicate_names(capsys: CaptureFixture[str]) -> None:
+    expected = 'Duplicates found'
+    participant = Player('jyrki', 'heikkinen')
+    with pytest.raises(SystemExit) as exit_msg:
+        rating_list.search_unique(participant, only_name=True)
+    output = capsys.readouterr()
+    assert expected in output.err or expected in output.out or expected in str(exit_msg.value)
