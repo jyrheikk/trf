6664622aba96ed599c21f2dbb56a4da4ba54ce7b
Jyrki Heikkinen <jyrki.j.heikkinen@gmail.com>
2026-09-25 23:16:51 +0300

    fix: encode non-Unicode characters

--- trf/player.py
+++ trf/player.py
@@ -2,6 +2,8 @@ from trf.club import parse_club
 
 DEFAULT_RATING = '1525'
 
+SINGLE_QUOTE = '\u0092'
+
 class Player:
     def __init__(self, first_name: str, last_name: str, rating = '', club = ''):
         self.first_name = first_name
@@ -14,12 +16,20 @@ class Player:
 
     def __set_combined_fields(self) -> None:
         optional_first_name = f', {self.first_name}' if self.first_name else ''
-        self.name = f'{self.last_name}{optional_first_name}'
+        self.name = self.human_readable(f'{self.last_name}{optional_first_name}')
         optional_club = f' ({self.club})' if self.club else ''
         self.name_club = f'{self.name}{optional_club}'
         self.name_rating = f'{self.name_club} {self.rating}'
-        self.__search_name = self.name.lower()
-        self.__search_name_club = self.name_club.lower()
+        self.__search_name = self.encode_search_name(self.name.lower())
+        self.__search_name_club = self.encode_search_name(self.name_club.lower())
+
+    @staticmethod
+    def human_readable(name: str) -> str:
+        return name.replace(SINGLE_QUOTE, "'")
+
+    @staticmethod
+    def encode_search_name(name: str) -> str:
+        return name.replace("'", SINGLE_QUOTE)
 
     def __eq__(self, p: Player) -> bool:
         return self.is_namesake(p) and self.rating == p.rating
